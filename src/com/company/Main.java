bb7ec134f2e55d664cdb74433e88420f89465e6b
user <maxter567terry@gmail.com>
2021-11-04 00:22:46 +0300

    d

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int age = 19;
+        String vvod = "Меня зовут ";
+        String vvod1 = "Мне ";
+        String name = "Максим.";
+        String hobby = "Я увлекаюсь версткой сайтов, программированием на многих языках программирования";
+        String hobby1 = "в том числе и Java, а также C#, C++, Phyton и тому подобное.";
+        String interest = "Из интересов в курсе могу выделить то, что я решил изучить новый";
+        String interest2 = "более актуальный язык программирования как для общего понимания, так и для посвящения данному языку работы!";
+        String work = "А на данный момент работаю фрилансером, то есть ищу заказы на биржах и тем самым работаю на себя.";
+        String embarrassment = "Больше всего меня смущает и неустраивает то, что с таким родом деятельности много не заработаешь.";
+        String embarrassment1 = "В целом выходит не более 30-40 тысяч рублей в месяц.";
+        System.out.println(vvod + name + "\n" + vvod1 + age + ".\n" + hobby + ", " + hobby1 + "\n" + interest + ", " + interest2 + "\n" +
+                work + "\n" + embarrassment + "\n" + embarrassment1);
+    }
+}
